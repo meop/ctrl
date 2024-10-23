f8bcc77d89b526a115cbe434b316595bf1e59659
Marshall Porter <meoporter@gmail.com>
2024-10-23 00:34:01 -0700

    cleaner compare

--- src/me.rs
+++ src/me.rs
@@ -9,9 +9,9 @@ use reqwest;
 use version_compare::Cmp;
 
 use crate::file::get_cur_path_str;
-use crate::log::Category;
 use crate::log::logcln;
 use crate::log::logln;
+use crate::log::Category;
 
 #[derive(Subcommand)]
 pub(super) enum Command {
@@ -87,18 +87,19 @@ fn upgrade() -> Result<(), Box<dyn Error>> {
     }
 
     let current_version = self_update::cargo_crate_version!();
-    let latest_version = &releases[0].version;
+    let mut latest_version = releases[0].version.as_str();
 
-    logcln(
-        &format!("current: {current_version}"),
-        Category::Info,
-    );
+    let binary = format!("{}-ctrl", self_update::get_target());
+    let asset = releases[0].asset_for(&binary, None);
+    if asset.is_none() {
+        latest_version = current_version;
+    }
+
+    logcln(&format!("current: {current_version}"), Category::Info);
     logcln(&format!("latest: {latest_version}"), Category::Info);
 
     if version_compare::compare_to(current_version, latest_version, Cmp::Ge).unwrap() {
-        logln(
-            "already at latest",
-        );
+        logln("already at latest");
         return Ok(());
     }
 
@@ -110,35 +111,25 @@ fn upgrade() -> Result<(), Box<dyn Error>> {
         return Ok(());
     }
 
-    let binary = format!("{}-ctrl", self_update::get_target());
+    let cur_path_str = get_cur_path_str()?;
 
-    if let Some(asset) = releases[0].asset_for(&binary, None) {
-        let cur_path_str = get_cur_path_str()?;
+    let bak_path_str = get_bak_path_str(&cur_path_str);
+    let tmp_path_str = get_tmp_path_str(&cur_path_str);
 
-        let bak_path_str = get_bak_path_str(&cur_path_str);
-        let tmp_path_str = get_tmp_path_str(&cur_path_str);
+    // copying preserves fs permissions
+    copy(&cur_path_str, &tmp_path_str)?;
 
-        // copying preserves fs permissions
-        copy(&cur_path_str, &tmp_path_str)?;
+    // create will then truncate the existing file
+    let tmp_path_file = File::create(&tmp_path_str)?;
 
-        // create will then truncate the existing file
-        let tmp_path_file = File::create(&tmp_path_str)?;
+    self_update::Download::from_url(&asset.unwrap().download_url)
+        .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
+        .download_to(&tmp_path_file)?;
 
-        self_update::Download::from_url(&asset.download_url)
-            .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
-            .download_to(&tmp_path_file)?;
-
-        self_update::Move::from_source(Path::new(&tmp_path_str))
-            // windows requires this; unix is optional
-            .replace_using_temp(Path::new(&bak_path_str))
-            .to_dest(Path::new(&cur_path_str))?;
-    } else {
-        logcln(
-            &format!("release missing: {binary}"),
-            Category::Info,
-        );
-        return Ok(());
-    }
+    self_update::Move::from_source(Path::new(&tmp_path_str))
+        // windows requires this; unix is optional
+        .replace_using_temp(Path::new(&bak_path_str))
+        .to_dest(Path::new(&cur_path_str))?;
 
     Ok(())
 }
